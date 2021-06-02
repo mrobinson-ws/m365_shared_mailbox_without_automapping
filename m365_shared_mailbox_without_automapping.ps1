bbc892d5f80c2b79f1c9f74920697b635007ee89
mrobinson-ws <84938976+mrobinson-ws@users.noreply.github.com>
2021-06-02 09:07:44 -0400

    Create m365_shared_mailbox_without_automapping.ps1

---
+++ m365_shared_mailbox_without_automapping.ps1
@@ -0,0 +1,30 @@
+#Requires -Modules AzureAD, ExchangeOnlineManagement
+
+# Test And Connect To AzureAD If Needed
+try {
+    Write-Verbose -Message "Testing connection to Azure AD"
+    Get-AzureAdDomain -ErrorAction Stop | Out-Null
+    Write-Verbose -Message "Already connected to Azure AD"
+}
+catch {
+    Write-Verbose -Message "Connecting to Azure AD"
+    Connect-AzureAD
+}
+
+#Test And Connect To Microsoft Exchange Online If Needed
+try {
+    Write-Verbose -Message "Testing connection to Microsoft Exchange Online"
+    Get-Mailbox -ErrorAction Stop | Out-Null
+    Write-Verbose -Message "Already connected to Microsoft Exchange Online"
+}
+catch {
+    Write-Verbose -Message "Connecting to Microsoft Exchange Online"
+    Connect-ExchangeOnline
+}
+
+$users = Get-AzureADUser | Sort-Object DisplayName | Select-Object -Property DisplayName,UserPrincipalName | Out-Gridview -Passthru -Title "Please select the user(s) to Share The Mailbox with" | Select-Object -ExpandProperty UserPrincipalName
+$sharedmailbox = Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize:Unlimited | Sort-Object DisplayName | Out-GridView -Title "Please select the mailbox you are adding the user(s) to" -OutputMode Single | Select-Object -ExpandProperty UserPrincipalName
+
+foreach ($user in $users) {
+    Add-MailboxPermission -Identity $sharedmailbox -User $user -AccessRights FullAccess -AutoMapping:$false
+}
